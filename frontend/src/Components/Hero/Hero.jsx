668d97c994f9a968dddac3d8b4d70977ab0be548
ashrafali2000 <ali4282271@gmail.com>
2024-05-28 10:29:46 +0500

    create navbar with router

---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./Hero.css";
+export const Hero = () => {
+  return <div className="hero">Hero</div>;
+};
---
+++ frontend/src/Pages/LoginSignUp.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LoginSignUp = () => {
+  return <div>LoginSignUp</div>;
+};
